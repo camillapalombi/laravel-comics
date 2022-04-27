cb7c95ab68ce752b3b3920d40743e90f4368743c
camillapalombi <camilla.palombi14@gmail.com>
2022-04-27 19:52:47 +0200

    Continuo progetto

--- laravel-comics/resources/views/visitors/partials/footer.blade.php
+++ laravel-comics/resources/views/visitors/partials/footer.blade.php
@@ -9,43 +9,42 @@
 
         <div class="dc-comics-links">
           <div class="title-links">DC COMICS</div>
-          <a href="#!">C</a>
-          <a href="#!">C</a>
-          <a href="#!">C</a>
-          <a href="#!">C</a>
-          <a href="#!">C</a>
-          <a href="#!">C</a>
-          <a href="#!">C</a>
+          <a href="#!">Characters</a>
+          <a href="#!">Comics</a>
+          <a href="#!">Videos</a>
+          <a href="#!">News</a>
+          <a href="#!">Shop</a>
+          <a href="#!">Movies</a>
+          <a href="#!">Tv</a>
         </div>
 
         <div class="dc-comics-links">
           <div class="title-links">DC</div>
-          <a href="#!">S</a>
-          <a href="#!">S</a>
+          <a href="#!">Terms of Use</a>
+          <a href="#!">Ad choices</a>
+          <a href="#!">Advertising</a>
+          <a href="#!">Jobs</a>
+          <a href="#!">Subscripton</a>
+          <a href="#!">Talent Workshop</a>
+          <a href="#!">CPSC Certificates</a>
+          <a href="#!">Ratings</a>
+          <a href="#!">Shop Help</a>
+          <a href="#!">Contact Us</a>
         </div>
 
         <div class="dc-comics-links">
-          <div class="title-links">SITE</div>
-          <a href="#!">D</a>
-          <a href="#!">D</a>
-          <a href="#!">D</a>
-          <a href="#!">D</a>
-          <a href="#!">D</a>
-          <a href="#!">D</a>
-          <a href="#!">D</a>
-          <a href="#!">D</a>
-          <a href="#!">D</a>
-          <a href="#!">D</a>
-          <a href="#!">D</a>
+          <div class="title-links">SITES</div>
+          <a href="#!">DC</a>
+          <a href="#!">MAD Magazine</a>
+          <a href="#!">DC Kids</a>
+          <a href="#!">DC Universe</a>
+          <a href="#!">DC Power Visa</a>
         </div>
 
         <div class="dc-comics-links shop-links">
           <div class="title-links">SHOP</div>
-          <a href="#!">A</a>
-          <a href="#!">A</a>
-          <a href="#!">A</a>
-          <a href="#!">A</a>
-          <a href="#!">A</a>
+          <a href="#!">Shop DC</a>
+          <a href="#!">Shop DC Collectibles</a>
         </div>
 
       </div>
